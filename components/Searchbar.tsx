e6fa70daa128f35d6c58d4140333397b6b61520c
Bat Orshikh <batorshikh@Bats-MacBook-Pro.local>
2024-05-17 17:48:55 +0800

    ds

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
-import SearchManufacturer from "./SearchManufacturer";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
@@ -61,10 +60,6 @@ const SearchBar = () => {
   return (
     <form className='searchbar' onSubmit={handleSearch}>
       <div className='searchbar__item'>
-        <SearchManufacturer
-          manufacturer={manufacturer}
-          setManuFacturer={setManuFacturer}
-        />
         <SearchButton otherClasses='sm:hidden' />
       </div>
       <div className='searchbar__item'>
